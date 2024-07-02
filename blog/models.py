24797ff23f1a38cc52e993b03f4beb798d4cca38
Gustavo Sales <dev.gustavosales01@gmail.com>
2024-07-02 20:50:16 -0300

    adicionado model Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    content = HTMLField()
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
