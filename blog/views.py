af4f1aac2f5bf67a86d4856569cc827844d8bf99
Gustavo Sales <dev.gustavosales01@gmail.com>
2024-07-02 20:59:43 -0300

    add view Index

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Post
+
+
+class Index(generic.ListView):
+    model = Post
+    queryset = Post.objects.all().order_by("-pub_date")
