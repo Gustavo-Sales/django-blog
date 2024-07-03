af59be6a0b6f993b944f017e5558c6439f26a77f
Gustavo Sales <dev.gustavosales01@gmail.com>
2024-07-02 21:11:31 -0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 
+app_name = "blog"
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
     path("tinymce/", include("tinymce.urls")),
