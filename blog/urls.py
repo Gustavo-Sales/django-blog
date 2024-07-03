72ae3336ee014de050ecd4c867a8fef2cb391724
Gustavo Sales <dev.gustavosales01@gmail.com>
2024-07-02 21:06:08 -0300

    add urls.py and urls for index and tinymce

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.Index.as_view(), name="index"),
+    path("tinymce/", include("tinymce.urls")),
+]
