44276f105a133fe65b77317d128314ba095645f3
Gustavo Sales <dev.gustavosales01@gmail.com>
2024-07-04 00:02:22 -0300

    add detail post view

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,8 @@ class Index(generic.ListView):
     queryset = Post.objects.all().order_by("-pub_date")
     context_object_name = "latest_posts_list"
     template_name = "index.html"
+
+
+class DetailPost(generic.DetailView):
+    model = Post
+    template_name = "detail_post.html"
