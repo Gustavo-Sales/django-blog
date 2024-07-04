b2c0f51fffc30dd82b7fa91a05bc9102b6129030
Gustavo Sales <dev.gustavosales01@gmail.com>
2024-07-03 23:11:57 -0300

    add templates

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,5 @@ from .models import Post
 class Index(generic.ListView):
     model = Post
     queryset = Post.objects.all().order_by("-pub_date")
+    context_object_name = "latest_posts_list"
+    template_name = "index.html"
